94a6bd8d556c6bdaa5e634f128f972878a674ef5
NickC25C <niccm25@gmail.com>
2024-06-10 12:43:25 +0200

    hice la segunda ventana incompleta

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,12 +1,19 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 1440,
     height: 1024,
+    webPreferences: {
+      contextIsolation: false,
+      //preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+    },
+    autoHideMenuBar: true,
   });
 
-  mainWindow.loadFile("index.html");
+  mainWindow.loadFile("pages/visualizar_page.html");
 }
 
 app.whenReady().then(createWindow);
